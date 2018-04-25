3c3a9a3232eb146f2664fdc3519cfb3dc1f1656e
Ivan Teslovych <i.teslovich@mobilunity.com>
2018-04-25 19:23:25 +0300

    Init posts render

--- src/components/posts.component.js
+++ src/components/posts.component.js
@@ -1,13 +1,38 @@
+import _ from 'lodash';
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPosts} from '../actions';
 
 class PostsIndexComponent extends Component {
+
+    componentDidMount() {
+        this.props.fetchPosts();
+    }
+
     render() {
-        return(
+        return (
             <div>
-                Posts
+                <h3>Posts</h3>
+                <ul className="list-group">
+                    {this.renderPosts()}
+                </ul>
             </div>
         );
     }
+
+    renderPosts() {
+        return _.map(this.props.posts, post => {
+            return (
+                <li className="list-group-item" key={post.id}>
+                    {post.title}
+                </li>
+            );
+        });
+    }
 }
 
-export default PostsIndexComponent;
+function mapStateToProps(state) {
+    return {posts: state.posts};
+}
+
+export default connect(mapStateToProps, {fetchPosts})(PostsIndexComponent);
