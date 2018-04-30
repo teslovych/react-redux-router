79b8fb4d5e00c10d13ea7d32e03a8f951a913355
Ivan Teslovych <i.teslovich@mobilunity.com>
2018-04-30 18:41:25 +0300

    Add redux-form

--- src/components/new-post.component.js
+++ src/components/new-post.component.js
@@ -1,13 +1,28 @@
 import React, {Component} from 'react';
+import {Field, reduxForm} from 'redux-form';
 
 class NewPostComponent extends Component {
+
+    renderField(field) {
+        return (
+            <div className="form-group">
+                <label>{field.label}</label>
+                <input className="form-control" {...field.input} type="text"/>
+            </div>
+        )
+    }
+
     render() {
         return (
-            <div>
-                New post
-            </div>
+            <form>
+                <Field name="title" label="Title" component={this.renderField}/>
+                <Field name="tags" label="Tags" component={this.renderField}/>
+                <Field name="content" label="Content" component={this.renderField}/>
+            </form>
         );
     }
 }
 
-export default NewPostComponent;
+export default reduxForm({
+    form: 'PostsNewForm'
+})(NewPostComponent);
