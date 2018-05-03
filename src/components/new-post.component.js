9ddfe22c0a304a256070c4b1abd65941b2463945
Ivan Teslovych <i.teslovich@mobilunity.com>
2018-05-03 18:51:32 +0300

    Add fields validation

--- src/components/new-post.component.js
+++ src/components/new-post.component.js
@@ -8,21 +8,39 @@ class NewPostComponent extends Component {
             <div className="form-group">
                 <label>{field.label}</label>
                 <input className="form-control" {...field.input} type="text"/>
+                {field.meta.touched ? field.meta.error: ''}
             </div>
         )
     }
 
+    onSubmit(values) {
+
+    }
+
     render() {
+        const {handleSubmit} = this.props;
+
         return (
-            <form>
+            <form onSubmit={handleSubmit(this.onSubmit.bind(this))}>
                 <Field name="title" label="Title" component={this.renderField}/>
                 <Field name="tags" label="Tags" component={this.renderField}/>
                 <Field name="content" label="Content" component={this.renderField}/>
+
+                <button type="submit" className="btn btn-primary">Submit</button>
             </form>
         );
     }
 }
 
+function validate(values) {
+    const errors = {};
+    if (!values.title) {
+        errors.title = 'Enter a title';
+    }
+    return errors;
+}
+
 export default reduxForm({
+    validate,
     form: 'PostsNewForm'
 })(NewPostComponent);
