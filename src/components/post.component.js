3e34e5aa781ca0e69c07a8a445e901b0c924cd76
teslovych <ivan.teslovich@gmail.com>
2018-06-11 23:06:31 +0300

    Fetch single post and bind to post component

--- src/components/post.component.js
+++ src/components/post.component.js
@@ -1,13 +1,33 @@
 import React, {Component} from 'react';
+import {connect} from 'react-redux';
+import {fetchPost} from '../actions';
 
 class PostComponent extends Component {
+
+    componentDidMount() {
+        const {id} = this.props.match.params;
+        this.props.fetchPost(id);
+    }
+
     render() {
+        const {post} = this.props;
+
+        if (!post) {
+            return (<div>Loading...</div>);
+        }
+
         return (
-         <div>
-             Post Page
-         </div>
+            <div>
+                <h3>{post.title}</h3>
+                <h6>Tags: {post.categories}</h6>
+                <p>{post.content}</p>
+            </div>
         )
     }
 }
 
-export default PostComponent;
+function mapStateToProps({posts}, ownProps) {
+    return {post: posts[ownProps.match.params.id]};
+}
+
+export default connect(mapStateToProps, {fetchPost})(PostComponent);
